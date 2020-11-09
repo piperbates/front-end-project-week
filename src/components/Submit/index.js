708b72e747757cdd31669d60938c054c35aff4f3
soupanova <duykhangkhuat@gmail.com>
2020-11-09 16:29:38 +0000

    components built

---
+++ src/components/Submit/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Submit component will send the users answer to our database via onClick.
+function Submit() {
+  return (
+    <button>onClick This will submit the users onChange input value</button>
+  );
+}
+
+export default Submit;
