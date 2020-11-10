2c7f86c47c0fa6d5fa3bf8ebfb2d22191b09b483
= <za.qa@outlook.com>
2020-11-10 14:30:45 +0000

    merging quick

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Mentor from "../Mentor";
-import Mentee from "../Mentee";
-import Submit from "../Submit";
 
 // Parent component of Mentor, Mentee, submit. In here we will take the information from Mentor & Mentee and the submit component will pass the data to our database
 function Form() {
   return (
-    <form>
-      this will store our mentor/mentee components
-      <Mentor />
-      <Mentee />
-      <Submit />
-    </form>
+    <div>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
   );
 }
 
