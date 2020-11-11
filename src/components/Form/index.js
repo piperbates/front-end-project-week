c0e2c2c9a01f7c9c3bd8a5bb28ceec62d3885996
= <za.qa@outlook.com>
2020-11-11 09:48:49 +0000

    Fetch functions ready

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -8,6 +8,58 @@ function Form() {
   const [menteeForm, setMenteeForm] = useState("form hide");
   const [mentorForm, setMentorForm] = useState("form hide");
 
+  //Getting input value and save to variable Mentee
+  const [teeForm1, setTeeForm1] = useState("");
+  const [teeForm2, setTeeForm2] = useState("");
+  const [teeForm3, setTeeForm3] = useState("");
+  const [teeForm4, setTeeForm4] = useState("");
+
+  //Getting input value and save to variable Mentor
+  const [torForm1, setTorForm1] = useState("");
+  const [torForm2, setTorForm2] = useState("");
+  const [torForm3, setTorForm3] = useState("");
+  const [torForm4, setTorForm4] = useState("");
+  const [torForm5, setTorForm5] = useState("");
+  console.log("log", torForm1, torForm2, torForm3, torForm4, torForm5);
+  //Submut mentee function
+  function postMenteeForm(teeForm1, teeForm2, teeForm3, teeForm4) {
+    console.log("mentee fnc", teeForm1, teeForm2, teeForm3, teeForm4);
+    fetch(`http://localhost:5000/mentee`, {
+      method: "post",
+      body: JSON.stringify({
+        mentee: teeForm1,
+        mentee: teeForm2,
+        mentee: teeForm3,
+        mentee: teeForm4,
+      }),
+      headers: { "Content-Type": "application/json" },
+      //Validation: ContentType
+    })
+      .then((res) => res.json()) //res.json() is an async function
+      .then((data) => console.log(data, "here's the data, buddy boy")) //In the browser
+      .catch((error) => console.log(error, "my error")); //uncaught promise rejection. The promise throws and error and I need to catch otherwise it will be thrown into the ether
+  }
+
+  //Submut mantor function
+  function postMentorForm(torForm1, torForm2, torForm3, torForm4, torForm5) {
+    console.log("mentee fnc", torForm1, torForm2, torForm3, torForm4, torForm5);
+    fetch(`http://localhost:5000/mentor`, {
+      method: "post",
+      body: JSON.stringify({
+        mentor: torForm1,
+        mentor: torForm2,
+        mentor: torForm3,
+        mentor: torForm4,
+      }),
+      headers: { "Content-Type": "application/json" },
+      //Validation: ContentType
+    })
+      .then((res) => res.json()) //res.json() is an async function
+      .then((data) => console.log(data, "here's the data, buddy boy")) //In the browser
+      .catch((error) => console.log(error, "my error")); //uncaught promise rejection. The promise throws and error and I need to catch otherwise it will be thrown into the ether
+  }
+
+  //Show and hide functions
   function showMentee() {
     if (mentorForm === "form") {
       setMentorForm("form hide");
@@ -69,32 +121,54 @@ function Form() {
             <label for="fname">Name:</label>
             <br />
             <br />
-            <input type="text" id="fname" name="fname" />
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              onChange={(e) => setTeeForm1(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">Age:</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTeeForm2(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">Ethnicity</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTeeForm3(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">MyersBriggs:</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTeeForm4(e.target.value)}
+            />
           </div>
 
           <div className="field">
-            <button className="btnsForm">Submit!</button>
+            <button className="btnsForm" onClick={postMenteeForm}>
+              Submit!
+            </button>
           </div>
         </form>
       </div>
@@ -115,39 +189,66 @@ function Form() {
             <label for="fname">Name:</label>
             <br />
             <br />
-            <input type="text" id="fname" name="fname" />
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              onChange={(e) => setTorForm1(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">Age:</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTorForm2(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">Ethnicity</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTorForm3(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">MyersBriggs:</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTorForm4(e.target.value)}
+            />
           </div>
 
           <div className="field">
             <label for="lname">Experience:</label>
             <br />
             <br />
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              onChange={(e) => setTorForm5(e.target.value)}
+            />
           </div>
 
           <div className="field">
-            <button className="btnsForm">Submit!</button>
+            <button className="btnsForm" onClick={postMentorForm}>
+              Submit!
+            </button>
           </div>
         </form>
       </div>
