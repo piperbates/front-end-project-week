b3336b152643db78b32e00b2618a76a354ab9a66
Piper <bates.sarah42@gmail.com>
2020-11-11 14:47:37 +0000

    displaying data

--- src/components/DisplayTable/index.js
+++ src/components/DisplayTable/index.js
@@ -3,32 +3,14 @@ import { isCompositeComponent } from "react-dom/test-utils";
 import "./style.css";
 //This component will display the data from the data
 
-function DisplayTable({ payload }) {
-  console.log(payload);
+function DisplayTable({ name, age, ethnicity, myersBriggs }) {
   return (
-    <div>
-      {/* <p>{quote.name}</p> */}
-      <table id="data-display">
-        <thead>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Ethnicity</th>
-          <th>Myers Briggs</th>
-        </thead>
-        <tbody>
-          {/* {payload.map((item) => {
-            return (
-              <tr>
-                <td>{item.name}</td>
-                <td>{item.age}</td>
-                <td>{item.ethnicity}</td>
-                <td>{item.myersBriggs}</td>
-              </tr>
-            );
-          })} */}
-        </tbody>
-      </table>
-    </div>
+      <tr>
+        <td>{name}</td>
+        <td>{age}</td>
+        <td>{ethnicity}</td>
+        <td>{myersBriggs}</td>
+      </tr>
   );
 }
 
--- src/components/RetrieveData/index.js
+++ src/components/RetrieveData/index.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DisplayTable from "../DisplayTable";
 
 // component is working without useEffect because it will only grab all the data.
 // As we add more button (to find your match) then useeffect we will want them to run at different times.
 function RetrieveData() {
-  const [formData, setFormData] = useState();
+  const [formData, setFormData] = useState([]);
 
   async function getAllData() {
     const res = await fetch("http://www.localhost:5000/mentor");
     const data = await res.json();
     let payload = data.payload;
-    console.log(payload[0]);
+    console.log(payload);
     setFormData(payload);
   }
   // function handleClick() {
@@ -19,12 +19,33 @@ function RetrieveData() {
 
   return (
     <div>
-    
-    <p>Example Test for the Retrieve Data comp</p>
+      <p>Example Test for the Retrieve Data comp</p>
+
       <button onClick={getAllData}>Get all data</button>
-   
-     <DisplayTable payload={formData} />
-      
+      <div>
+        {/* <p>{quote.name}</p> */}
+        <table id="data-display">
+          <thead>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Ethnicity</th>
+            <th>Myers Briggs</th>
+          </thead>
+          <tbody>
+            {formData && formData.map((item) => {
+              console.log(item.name)
+              return (
+              <DisplayTable
+                name={item.name}
+                age={item.age}
+                ethnicity={item.ethnicity}
+                myersBriggs={item.myersbriggs}
+              />)
+              
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
