266bbb946c595c873ca73ce0d9f8478173cdcfa7
Ismail Ali <ismailali@ismails-mbp.home>
2020-11-11 10:46:35 +0000

    ish branch

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,19 +1,36 @@
 import React from "react";
 import "./styles.css";
+import Form from "../Form"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="nav">
-      <button className="btnNav" href="#">
+    <Router>
+    <div>
+    <Link to="/"><button className="btnNav" href="#">
         Apply for programme
-      </button>
-      <button className="btnNav" href="#">
+      </button></Link>
+      <Link to="/FindYourMatch"><button className="btnNav" href="#">
         {" "}
-        Find your Match
-      </button>
-      <button className="btnNav" href="#">
-        Chat
-      </button>
+        Find Your Match 
+      </button></Link>
+      <Link to="/Chat"><button className="btnNav" href="#">
+        Chat 
+      </button></Link>
+      <Switch>
+          <Route path="/">
+            <Form />
+          </Route>
+          </Switch>
+        </div>
+    </Router>
     </nav>
   );
 }
