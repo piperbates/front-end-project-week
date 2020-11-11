dd475c766912b194737d64dc95c48c323d1d1ce4
Piper <bates.sarah42@gmail.com>
2020-11-11 10:47:45 +0000

    fixing the nav part 1

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,15 @@ import "./App.css";
 import Form from "../Form";
 import RetrieveData from "../RetrieveData";
 import Footer from "../Footer";
+import Nav from "../Nav"
 
 function App() {
   return (
     <div className="App">
+    <Nav />
       {/* <Home /> */}
-      <Form />
       {/* <RetrieveData /> */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
