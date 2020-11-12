3b95b872a15fe009e93d344d59949f06132aafaa
Kunmi Williams <kunmiwilliams@Kunmis-MacBook-Pro-2.local>
2020-11-12 12:17:43 +0000

    Loginpage is home

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,16 +2,25 @@ import "./App.css";
 import Form from "../Form";
 //import RetrieveData from "../RetrieveData";
 import Footer from "../Footer";
-import Nav from "../Nav"
+import Nav from "../Nav";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginPage from "../LoginPage";
 
 function App() {
-  return (
-    <div className="App">
-    <Nav />
-    
-      <Footer />
-    </div>
-  );
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  if (isAuthenticated) {
+    return (
+      <div>
+        <div className="App">
+          <Nav />
+
+          <Footer />
+        </div>
+      </div>
+    );
+  } else {
+    return <LoginPage />;
+  }
 }
 
 export default App;
---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import LoginButton from "../LoginButton";
+import LogoutButton from "../LogoutButton";
+import Profile from "../Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function LoginPage() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default LoginPage;
+
+// LogIn page should link to the Apply for programme page.... if user has already applied for programme then display User profile information
+// fetch request from the DB for the id of that USER.
